52f88913cd5b24661b02faafdf739fb2911012cb
kang <dlwprkdlq@naver.com>
2025-03-06 05:43:23 +0900

    Add: MainActivity 네비게이션 연결

--- app/src/main/java/com/app/ui/activity/MainActivity.kt
+++ app/src/main/java/com/app/ui/activity/MainActivity.kt
@@ -3,6 +3,10 @@ package com.app.ui.activity
 import android.content.Intent
 import androidx.activity.viewModels
 import androidx.lifecycle.LifecycleOwner
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.app.R
 import com.app.databinding.ActivityMainBinding
 import com.core.ui.BaseActivity
 import com.presentation.MainViewModel
@@ -12,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
     private val viewModel: MainViewModel by viewModels()
     override fun setUi() {
+        bindingNavigation()
     }
 
     override fun setUpDate() {
@@ -20,6 +25,20 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
 
     override fun setObserve(lifecycleOwner: LifecycleOwner) {
         viewModel.checkLoggedInValue.observe(lifecycleOwner, ::checkLoggedIn)
+    }
+
+    private fun bindingNavigation() {
+        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController() ?: return
+        binding.bottomNavigation.setupWithNavController(navController)
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_navi_profile, R.id.menu_navi_friends, R.id.menu_navi_chat, R.id.menu_navi_schedule -> {
+                    NavigationUI.onNavDestinationSelected(item, navController)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun checkLoggedIn(value: Boolean) {
