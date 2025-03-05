66fe3bf7b6c1cd6da442a3c31f591e81a618bcdb
kang <dlwprkdlq@naver.com>
2025-03-05 13:45:52 +0900

    Add: MapActivity 관련 내용 추가

--- app/src/main/java/com/app/ui/activity/SignUpActivity.kt
+++ app/src/main/java/com/app/ui/activity/SignUpActivity.kt
@@ -1,30 +1,43 @@
 package com.app.ui.activity
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.viewModels
 import androidx.lifecycle.LifecycleOwner
-import com.app.R
-import com.app.databinding.ActivityMainBinding
 import com.app.databinding.ActivitySignUpBinding
 import com.core.ui.BaseActivity
+import com.presentation.SignUpViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SignUpActivity : BaseActivity<ActivitySignUpBinding>(ActivitySignUpBinding::inflate) {
+    private val viewModel: SignUpViewModel by viewModels()
+    private val addressResultLauncher = registerForActivityResultHandler { result ->
+        if (result.resultCode == RESULT_OK) {
+            val zoneCode = result.data?.getStringExtra("zoneCode") ?: ""
+            val address = result.data?.getStringExtra("address") ?: ""
+            viewModel.setAddress(zoneCode, address)
+        }
+    }
+
     override fun setUi() {
+        binding.viewModel = viewModel
+        setBinding()
     }
 
     override fun setUpDate() {
-        startLoginActivity()
     }
 
     override fun setObserve(lifecycleOwner: LifecycleOwner) {
     }
 
-    private fun startLoginActivity(){
+    private fun setBinding() {
+        binding.btnSearch.setOnClickListener {
+            startAddressSearchActivity()
+        }
+    }
+
+    private fun startAddressSearchActivity() {
+        val intent = Intent(this, MapActivity::class.java)
+        addressResultLauncher.launch(intent)
     }
 }
---
+++ presentation/src/main/java/com/presentation/SignUpViewModel.kt
@@ -0,0 +1,26 @@
+package com.presentation
+
+import androidx.lifecycle.MutableLiveData
+import com.core.di.DefaultDispatcher
+import com.core.di.IoDispatcher
+import com.core.di.MainDispatcher
+import com.core.viewmodel.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.MainCoroutineDispatcher
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    @MainDispatcher mainDispatcher: MainCoroutineDispatcher,
+    @DefaultDispatcher defaultDispatcher: CoroutineDispatcher,
+    @IoDispatcher ioDispatcher: CoroutineDispatcher
+) : BaseViewModel(mainDispatcher, defaultDispatcher, ioDispatcher) {
+    val zoneCodeLiveData = MutableLiveData("")
+    val addressLiveData = MutableLiveData("")
+
+    fun setAddress(zonCode: String, address: String) {
+        zoneCodeLiveData.value = zonCode
+        addressLiveData.value = address
+    }
+}
